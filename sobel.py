d67be7e5feb902f43a72944debc1f91aff1e17cb
vivian <rtbn3y61@gmail.com>
2021-08-13 10:34:18 +0800

    sobel.py

---
+++ sobel.py
@@ -0,0 +1,21 @@
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('./image/img2.png',0)
+roi = img[144:500,560:800]
+
+sobelx = cv2.Sobel(roi,cv2.CV_8U,1,0,ksize=1)  #ksize=3 的值越小顯示越清楚 不能偶數
+sobely = cv2.Sobel(roi,cv2.CV_8U,0,1,ksize=1)
+#把 sobelx 和 sobely 兩張圖各別0.5加起來 
+sobel = cv2.addWeighted(sobelx,0.5,sobely,0.5,0)
+#把 sobelx 和 sobely 兩張圖直接加起來 
+sobel1 = cv2.add(sobelx,sobely)
+cv2.imshow('roi',roi)
+cv2.imshow('sobelx',sobelx)
+cv2.imshow('sobely',sobely)
+cv2.imshow('sobel',sobel)
+cv2.imshow('sobel ADD',sobel1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
